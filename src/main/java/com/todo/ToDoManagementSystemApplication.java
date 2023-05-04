21fa92700a07bb6c5aeab470e5b2bffd71669f8c
Sancean <246288805@qq.com>
2023-05-04 23:11:47 +0800

    docs(ToDoManagementSystemApplication): 添加作者信息

--- src/main/java/com/todo/ToDoManagementSystemApplication.java
+++ src/main/java/com/todo/ToDoManagementSystemApplication.java
@@ -3,9 +3,11 @@ package com.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Sancean
+ */
 @SpringBootApplication
 public class ToDoManagementSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ToDoManagementSystemApplication.class, args);
     }
